b3380a00a209673ffc5cc130c5d35135f50f8c74
Ilja Kristafenko <iljaKristafenko@gmail.com>
2021-09-19 22:16:23 +0300

    Home work - PrimitiveTypes.

---
+++ src/main/java/practis/PrimitiveTypes.java
@@ -0,0 +1,27 @@
+package practis;
+
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        int numbers = 10;
+        byte theBigestByte = 127;
+        short theBigestShort = 32767;
+        float PI = 3.14f;
+        double doubleNumber = 4444.999999;
+        boolean isOpen = true;
+        System.out.println(numbers);
+        System.out.println(theBigestByte);
+        System.out.println(theBigestShort);
+        System.out.println(PI);
+        System.out.println(doubleNumber);
+        System.out.println(isOpen);
+
+        //Hello this is Information about me.
+
+        String name = "Ilja";
+        String surname = "Kristafenko";
+        int dateOfBirth = 1988;
+        System.out.println("I was born in " + dateOfBirth + " My name is " + name + " surname: " + surname);
+
+    }
+}
